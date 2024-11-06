713320d17b42ad28c612dc565b0308e03f696d8d
Jennie <jenniean2002@gmail.com>
2024-11-06 14:17:04 -0500

    make code more modular

--- code/audio_CNN.py
+++ code/audio_CNN.py
@@ -1,4 +1,5 @@
 import torch
+import os
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
@@ -6,36 +7,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import TensorDataset, DataLoader
 import torch.nn.functional as F
 
-# load in audio data
-filename = 'C:/Users/jenni/virtualEnv/CS 5100/Final Project/Audio/features+labels.npy'
-
-with open(filename, 'rb') as f:
-    X_train = np.load(f)
-    X_val = np.load(f)
-    X_test = np.load(f)
-    y_train = np.load(f).astype(np.int64)
-    y_val = np.load(f).astype(np.int64)
-    y_test = np.load(f).astype(np.int64)
-
-# create Tensor datasets
-train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
-val_data = TensorDataset(torch.from_numpy(X_val), torch.from_numpy(y_val))
-test_data = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
-
-# bacth size set
-batch_size = 50
-
-# shuffling and batching data
-train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
-valid_loader = DataLoader(val_data, shuffle=True, batch_size=batch_size)
-test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
-
-# define parameters
-num_classes = 8
-dropout = 0.5
-lr = 0.0001
-
-# Audio CNN 
+# Audio CNN Class
 class Audio_CNN(nn.Module):
     def __init__(self, num_classes, dropout):
         super().__init__() 
@@ -85,25 +57,66 @@ class Audio_CNN(nn.Module):
         if self.fc1 is None:  
             input_size = x.size(1)  
             self.fc1 = nn.Linear(input_size, 256)  
-            
         # Pass through fully connected layers
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         
         return x
     
-# Instantiate the model
-net = Audio_CNN(num_classes=num_classes, dropout=dropout)
+def load_data(filename, batch_size):
+    """
+    Purpose: load in preprocesse data
+    Args:
+        filename: path to preprocessed MFCC audio data
+        batch_size: size of batches
+    Returns: train, validation, and test split data loaders
+    """
 
-# Loss and Optimizer
-optimizer = optim.Adam(net.parameters(), lr=lr)
-criterion = nn.CrossEntropyLoss()
+    with open(filename, 'rb') as f:
+        X_train = np.load(f)
+        X_val = np.load(f)
+        X_test = np.load(f)
+        y_train = np.load(f).astype(np.int64)
+        y_val = np.load(f).astype(np.int64)
+        y_test = np.load(f).astype(np.int64)
 
-# Device setup
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # create Tensor datasets
+    train_data = TensorDataset(torch.from_numpy(X_train), torch.from_numpy(y_train))
+    val_data = TensorDataset(torch.from_numpy(X_val), torch.from_numpy(y_val))
+    test_data = TensorDataset(torch.from_numpy(X_test), torch.from_numpy(y_test))
 
-# training loop
-def train(net, train_loader, epochs, print_every=10):
+    # shuffling and batching data
+    train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
+    valid_loader = DataLoader(val_data, shuffle=True, batch_size=batch_size)
+    test_loader = DataLoader(test_data, shuffle=True, batch_size=batch_size)
+    
+    return train_loader, valid_loader, test_loader
+
+def train(
+    save_dir,
+    net,
+    train_loader,
+    valid_loader,
+    device,
+    optimizer,
+    criterion,
+    epochs = 100,
+    print_every=10
+):
+    """
+    Purpose: Train the model
+    Args:
+        save_dir: directory to save trained model weights and biases and loss plot
+        net: model to train
+        train_loader: train data loader
+        valid_loader: validation data loader
+        device: device to run model on
+        optimizer: optimizer to use
+        criterion: loss function to use
+        epochs: training epoch count
+        print_every: print loss after number of batches
+    Returns: trained model
+    """
 
     counter = 0 
     epoch_train_loss = []
@@ -170,7 +183,7 @@ def train(net, train_loader, epochs, print_every=10):
         'model_state_dict': net.state_dict(),
         'optimizer_state_dict': optimizer.state_dict(),
         'loss': loss,
-    }, 'C:/Users/jenni/virtualEnv/CS 5100/Final Project/Audio/audio_CNN_model_checkpoint.pth')
+    }, os.path.join(save_dir, 'audio_CNN_model_checkpoint.pth'))
     
     # save train and val loss plot
     plt.plot(epoch_train_loss, label='Training Loss')
@@ -179,43 +192,99 @@ def train(net, train_loader, epochs, print_every=10):
     plt.ylabel('Loss')
     plt.legend(['Train', 'Val'], loc='upper left')
     plt.title('Training and Validation Loss over Epochs')
-    plt.savefig('C:/Users/jenni/virtualEnv/CS 5100/Final Project/Audio/Audio_loss_plot.png')
-
-# training params
-epochs = 100
-print_every = 10
-
-train(net, train_loader, epochs, print_every=print_every)
-
-# Get test data loss and accuracy
-
-test_losses = [] # track loss
-num_correct = 0
-
-net.eval()
-# iterate over test data
-for inputs, labels in test_loader:
-
-    inputs, labels = inputs.to(device), labels.to(device)
+    plt.savefig(os.path.join(save_dir, 'Audio_loss_plot.png'))
     
-    # get predicted outputs
-    output = net(inputs)
-    
-    # calculate loss
-    test_loss = criterion(output, labels)
-    test_losses.append(test_loss.item())
-    
-    # convert output probabilities to predicted class, get max prob class
-    pred = torch.argmax(output, dim=1) 
-    
-    # compare predictions to true label
-    correct_tensor = pred.eq(labels)
-    correct = np.squeeze(correct_tensor.numpy()) if device == 'cpu' else np.squeeze(correct_tensor.cpu().numpy())
-    num_correct += np.sum(correct)
+    return net  
 
-# avg test loss
-print(f"Test loss: {np.mean(test_losses)}")
+def eval(
+    test_loader,
+    net,
+    device,
+    criterion
+):
+    """
+    Purpose: Evaluate/test the model
+    Args:
+        test_loader: test data loader
+        net: trained model to evaluate
+        device: device to run eval on
+        criterion: loss function
+    Returns: test loss and accuracy
+    """
+    
+    # Get test data loss and accuracy
+    test_losses = [] # track loss
+    num_correct = 0
 
-# accuracy over all test data
-test_acc = num_correct / len(test_loader.dataset)
-print(f"Test accuracy: {test_acc}")
+    net.eval()
+    # iterate over test data
+    for inputs, labels in test_loader:
+
+        inputs, labels = inputs.to(device), labels.to(device)
+        
+        # get predicted outputs
+        output = net(inputs)
+        
+        # calculate loss
+        test_loss = criterion(output, labels)
+        test_losses.append(test_loss.item())
+        
+        # convert output probabilities to predicted class, get max prob class
+        pred = torch.argmax(output, dim=1) 
+        
+        # compare predictions to true label
+        correct_tensor = pred.eq(labels)
+        correct = np.squeeze(correct_tensor.numpy()) if device == 'cpu' else np.squeeze(correct_tensor.cpu().numpy())
+        num_correct += np.sum(correct)
+
+    # avg test loss
+    print(f"Test loss: {np.mean(test_losses)}")
+
+    # accuracy over all test data
+    test_acc = num_correct / len(test_loader.dataset)
+    print(f"Test accuracy: {test_acc}")
+    
+    return np.mean(test_losses), test_acc
+    
+def run_audio_cnn(
+    train_loader,
+    valid_loader,
+    test_loader,
+    save_dir,
+    dropout = 0.5,
+    lr = 0.0001,
+    epochs = 100,
+    print_every=10
+):
+    """
+    Purpose: Train and test the model
+    Args:
+        train_loader: train data loader
+        valid_loader: validation data loader
+        test_loader: test data loader
+        save_dir: directory to save trained model weights and biases and loss plot
+        dropout: dropout rate
+        lr: learning rate
+        epochs: train epochs
+        print_every: print loss every number of batches
+    Returns: None
+    """
+    
+    # define parameters
+    num_classes = 8
+
+    # Instantiate the model
+    net = Audio_CNN(num_classes=num_classes, dropout=dropout)
+
+    # Loss and Optimizer
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+    criterion = nn.CrossEntropyLoss()
+
+    # Device setup
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    trained_net = train(save_dir, net, train_loader, valid_loader, device,
+                        optimizer, criterion, epochs, print_every)
+    
+    print(eval(test_loader, trained_net, device, criterion))
+    
