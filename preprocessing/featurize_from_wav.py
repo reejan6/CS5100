88c046eaa7802195b069970ce6efb107ff0d590b
Jennie <jenniean2002@gmail.com>
2024-11-20 17:59:12 -0500

    reformat preprocessing

--- preprocessing/featurize_from_wav.py
+++ preprocessing/featurize_from_wav.py
@@ -3,6 +3,109 @@ import pandas as pd
 import os, glob
 import librosa
 
+def feature_mfcc(
+    waveform, 
+    sample_rate,
+    n_mfcc = 40,
+    fft = 1024,
+    winlen = 512,
+    window='hamming',
+    mels=128
+    ):
+
+    # Compute the MFCCs for all STFT frames 
+    mfc_coefficients=librosa.feature.mfcc(
+        y=waveform, 
+        sr=sample_rate, 
+        n_mfcc=n_mfcc,
+        n_fft=fft, 
+        win_length=winlen, 
+        window=window, 
+        n_mels=mels, 
+        fmax=sample_rate/2
+        ) 
+
+    return mfc_coefficients
+
+def get_features(waveforms, features, sample_rate):
+
+    # process each waveform individually to get its MFCCs
+    for waveform in waveforms:
+        mfccs = feature_mfcc(waveform, sample_rate)
+        features.append(mfccs)
+    
+    # return all features from list of waveforms
+    return features
+
+def get_waveforms(file):
+    
+    # load an individual sample audio file
+    # read the full 3 seconds of the file, cut off the first 0.5s of silence
+    # don't need to store the sample rate that librosa.load returns
+    waveform, _ = librosa.load(file, duration=3, offset=0.5, sr=sample_rate)
+    
+    # make sure waveform vectors are homogenous by defining explicitly
+    waveform_homo = np.zeros((int(sample_rate*3,)))
+    waveform_homo[:len(waveform)] = waveform
+    
+    # return a single file's waveform                                      
+    return waveform_homo
+
+def awgn_augmentation(waveform, multiples=2, bits=16, snr_min=15, snr_max=30): 
+    
+    # get length of waveform 
+    wave_len = len(waveform)
+    
+    # Generate normally distributed (Gaussian) noises
+    # one for each waveform and multiple (i.e. wave_len*multiples noises)
+    noise = np.random.normal(size=(multiples, wave_len))
+    
+    # Normalize waveform and noise
+    norm_constant = 2.0**(bits-1)
+    norm_wave = waveform / norm_constant
+    norm_noise = noise / norm_constant
+    
+    # Compute power of waveform and power of noise
+    signal_power = np.sum(norm_wave ** 2) / wave_len
+    noise_power = np.sum(norm_noise ** 2, axis=1) / wave_len
+    
+    # Choose random SNR in decibels in range [15,30]
+    snr = np.random.randint(snr_min, snr_max)
+    
+    # Apply whitening transformation: make the Gaussian noise into Gaussian white noise
+    covariance = np.sqrt((signal_power / noise_power) * 10 ** (- snr / 10))
+    # Get covariance matrix with dim: (144000, 2) so we can transform 2 noises: dim (2, 144000)
+    covariance = np.ones((wave_len, multiples)) * covariance
+
+    # Since covariance and noise are arrays, * is the haddamard product 
+    # Take Haddamard product of covariance and noise to generate white noise
+    multiple_augmented_waveforms = waveform + covariance.T * noise
+    
+    return multiple_augmented_waveforms
+
+def augment_waveforms(waveforms, features, emotions, multiples):
+    # keep track of how many waveforms we've processed so we can add correct emotion label in the same order
+    emotion_count = 0
+    # convert emotion array to list for more efficient appending
+    emotions = emotions.tolist()
+
+    for waveform in waveforms:
+
+        # Generate 2 augmented multiples of the dataset, i.e. 1440 native + 1440*2 noisy = 4320 samples total
+        augmented_waveforms = awgn_augmentation(waveform, multiples=multiples)
+
+        # compute spectrogram for each of 2 augmented waveforms
+        for augmented_waveform in augmented_waveforms:
+
+            # Compute MFCCs over augmented waveforms
+            augmented_mfcc = feature_mfcc(augmented_waveform, sample_rate=sample_rate)
+
+            # append the augmented spectrogram to the rest of the native data
+            features.append(augmented_mfcc)
+            emotions.append(emotions[emotion_count])
+    
+    return features, emotions
+
 def featurize_from_wav(wav_path):
     sample_rate = 48000
     
